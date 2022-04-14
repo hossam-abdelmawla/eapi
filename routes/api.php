182a7c986964dd04d0873bbeb523558965779c46
Hossam Abd EL-Mawla <hossammawladev@gmail.com>
2022-04-14 04:42:39 +0200

    Passport Install

--- routes/api.php
+++ routes/api.php
@@ -7,9 +7,13 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::apiResource('/products', ProductController::class);
 
